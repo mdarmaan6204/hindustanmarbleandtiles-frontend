8ecdac28d6a33f19e428fb0053388d9c2d917142
mdarmaan6204 <mdarmaan6204@gmail.com>
2025-11-19 20:28:41 +0530

    deploy fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build',
+    emptyOutDir: true,
+    sourcemap: false
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:5000'
